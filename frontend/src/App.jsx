b3145b6ed1f3249d061f1ee2c656055c51b01a94
Atari Ryuma <ryuma.atari@gmail.com>
2025-12-13 08:30:42 +0900

    Fix ToastProvider: move to App.jsx so TravelApp can use useToast

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
 import TravelApp from './components/TravelApp'
 import './App.css'
+import { ToastProvider } from './context/ToastContext'
 
 function App() {
   return (
-    <div className="w-full flex justify-center">
-      <TravelApp />
-    </div>
+    <ToastProvider>
+      <div className="w-full flex justify-center">
+        <TravelApp />
+      </div>
+    </ToastProvider>
   )
 }
 
